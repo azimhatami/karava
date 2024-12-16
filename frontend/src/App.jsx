908e8910c48c43da3ee48b98329e5d0e1501dd87
Azim Hatami <azimhatami4@gmail.com>
2024-12-16 11:47:47 +0330

    Install tailwindcss and configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Freelance Hub</h1>
+      <h1 className='container xl:max-w-screen-xl bg-primary-900 text-secondary-400 text-[2rem] font-bold'>فریلنسر هاب</h1>
     </>
   )
 }
