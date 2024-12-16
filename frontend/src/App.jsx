a09069350770f8aa83f680afc03ecd6745f8a510
Azim Hatami <azimhatami4@gmail.com>
2024-12-16 16:32:03 +0330

    Initial directory structure project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Auth from './pages/Auth';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className='container xl:max-w-screen-xl bg-primary-900 text-secondary-400 text-[2rem] font-bold'>فریلنسر هاب</h1>
+      <Routes>
+        <Route path='/auth' element={<Auth />} />
+      </Routes>
     </>
   )
 }
