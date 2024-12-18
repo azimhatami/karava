9575b59fe5f36f9d6177b13070c4dff72e46ff2d
Azim Hatami <azimhatami4@gmail.com>
2024-12-18 23:52:13 +0330

    Implemented completeProfile serviice

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -5,7 +5,10 @@ export function getOtp(data) {
   return http.post('/user/get-otp', data).then(({ data }) => data.data);
 }
 
-
 export function checkOtp(data) {
   return http.post('/user/check-otp', data).then(({ data }) => data.data);
+}
+
+export function completeProfile(data) {
+  return http.post('/user/complete-profile', data).then(({ data }) => data.data);
 }
