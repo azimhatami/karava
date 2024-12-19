a87e12cef5243250f80ec9efcdb3ab2254040767
Azim Hatami <azimhatami4@gmail.com>
2024-12-19 18:36:00 +0330

    Handling refresh token and access token

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -1,10 +1,37 @@
 import axios from 'axios';
 
 
+const BASE_URL = 'http://localhost:5000/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: BASE_URL,
   withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+)
+
+api.interceptors.response.use(
+  res => res,
+  async (err) => {
+    const originalConfig = err.config;
+    if (err.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await axios.get(`${BASE_URL}/user/refresh-token`, {
+          withCredentials: true
+        });
+        if (data) return api(originalConfig)
+
+      } catch(error) {
+        return Promise.reject(error)
+      }
+    }
+    return Promise.reject(err)
+  }
+)
 
 const http = {
   get: api.get,
