1b0f471285dfbf0f1ef4220c5612891acae29bbb
Azim Hatami <azimhatami4@gmail.com>
2024-12-24 11:18:43 +0330

    Implemented OutsideClick custom hook

--- frontend/src/ui/Modal.jsx
+++ frontend/src/ui/Modal.jsx
@@ -1,12 +1,17 @@
+import { useRef, useEffect } from 'react';
 import { HiOutlineX } from 'react-icons/hi';
+import useOutsideClick from '../hooks/useOutsideClick';
 
 
 function Modal({ open, onClose, title, children }) {
+
+  const ref = useOutsideClick(onClose);
+
   return open && (
     <div className='backdrop-blur-sm fixed top-0 left-0 w-full h-screen \
       bg-secondary-800/30 z-50'
     >
-      <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 \
+      <div ref={ref} className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 \
         rounded-lg bg-secondary-0 p-4 shadow-lg transition-all duration-500 \
         ease-out w-[calc(100vw-1.5rem)] md:max-w-lg max-h-[calc(100vh-2rem)] overflow-y-auto'
       >
