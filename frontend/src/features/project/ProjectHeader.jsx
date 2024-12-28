f552d8342cefd0a01ffe6e4d44940129ee62b1c0
Azim Hatami <azimhatami4@gmail.com>
2024-12-28 21:34:31 +0330

    Completed ProjectHeader component

--- frontend/src/features/project/ProjectHeader.jsx
+++ frontend/src/features/project/ProjectHeader.jsx
@@ -1,7 +1,15 @@
+import { HiArrowRight } from 'react-icons/hi';
+import useMoveBack from '../../hooks/useMoveBack';
+
+
 function ProjectHeader({ project }) {
+  const moveBack = useMoveBack();
   return(
-    <div>
-      Project header
+    <div className='flex gap-x-4 mb-8'>
+      <button onClick={moveBack}>
+        <HiArrowRight className='w-5 h-5 text-secondary-600 hover:text-secondary-900' />
+      </button>
+      <h2 className='font-black text-secondary-700 text-xl'>لیست درخواست های {project.title}</h2>
     </div>
   );
 }
