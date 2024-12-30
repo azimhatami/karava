ee273866c977c963f9f9b35fc68fe3338bf08e9c
Azim Hatami <azimhatami4@gmail.com>
2024-12-30 09:40:48 +0330

    Extract Header to HeaderMenu & UserAvatar component

--- frontend/src/ui/Header.jsx
+++ frontend/src/ui/Header.jsx
@@ -1,12 +1,20 @@
 import useUser from '../features/authentication/useUser';
+import HeaderMenu from './HeaderMenu';
+import UserAvatar from '../features/authentication/UserAvatar';
 
 
 function Header() {
-  const { data } = useUser();
+  const { user, isLoading } = useUser();
   return(
     <>
-      <div className='bg-secondary-0 py-4 px-8'>
-        Header
+      <div className='bg-secondary-0 py-4 px-8 border-b border-secondary-200'>
+        <div 
+          className={`container xl:max-w-screen-lg flex items-center justify-end 
+            \ gap-x-8 ${isLoading ? 'blur-sm opacity-50' : ''}`}
+        >
+          <UserAvatar />
+          <HeaderMenu />
+        </div>
       </div>
     </>
   );
