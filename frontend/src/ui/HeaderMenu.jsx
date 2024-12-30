75f6fee17adda6474581e801fc3d18be2d65d289
Azim Hatami <azimhatami4@gmail.com>
2024-12-30 12:24:10 +0330

    Completed HeaderMenu component

--- frontend/src/ui/HeaderMenu.jsx
+++ frontend/src/ui/HeaderMenu.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router';
+import { HiOutlineUser } from "react-icons/hi"; 
+import DarkModeToggle from './DarkModeToggle';
+import Logout from '../features/authentication/Logout';
+
+
 function HeaderMenu() {
   return(
     <>
-      HeaderMenu
+      <ul className='flex gap-x-4 items-center'>
+        <li>
+          <Link to='dashboard'>
+            <HiOutlineUser className='w-5 h-5 text-primary-900'/>
+          </Link>
+        </li>
+        <li>
+          <DarkModeToggle />
+        </li>
+        <li>
+          <Logout />
+        </li>
+      </ul>
     </>
   );
 }
