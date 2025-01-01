2cecdeb093fbf192a6b5b0dc2d97054dda11ed5a
Azim Hatami <azimhatami4@gmail.com>
2025-01-01 22:49:25 +0330

    Implemented proposal table

---
+++ frontend/src/features/proposals/ProposalTable.jsx
@@ -0,0 +1,34 @@
+import useProposals from './useProposals';
+import Loading from '../../ui/Loading';
+import Empty from '../../ui/Empty';
+import Table from '../../ui/Table';
+import ProposalRow from './ProposalRow';
+
+
+function ProposalTable() {
+  const { isLoading, proposals } = useProposals();
+
+  if (isLoading) return <Loading />;
+
+  if (!proposals.length) return <Empty resourceName='درخواست'/>;
+
+  return(
+    <Table>
+      <Table.Header>
+        <th>#</th>
+        <th>توضیحات</th>
+        <th>زمان تحویل</th>
+        <th>هزینه</th>
+        <th>وضعیت</th>
+      </Table.Header> 
+      <Table.Body>
+        {proposals.map((proposal, index) => (
+          <ProposalRow key={proposal._id} proposal={proposal} index={index} />
+        ))}
+      </Table.Body>
+    </Table>
+  );
+}
+
+
+export default ProposalTable
--- frontend/src/pages/Proposals.jsx
+++ frontend/src/pages/Proposals.jsx
@@ -1,8 +1,12 @@
+import ProposalTable from '../features/proposals/ProposalTable';
+
+
 function Proposals() {
   return(
-    <>
-      Proposals
-    </>
+    <div>
+      <h2 className='font-black text-secondary-700 text-xl mb-8'>درخواست ها</h2>
+      <ProposalTable />
+    </div>
   );
 }
 
