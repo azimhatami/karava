d1180408b240fda487b3f4d797dc923b3b0bf916
Azim Hatami <azimhatami4@gmail.com>
2025-01-03 18:49:01 +0330

    Implemented Filter compnent

--- frontend/src/features/freelancer/projects/ProjectsHeader.jsx
+++ frontend/src/features/freelancer/projects/ProjectsHeader.jsx
@@ -1,13 +1,42 @@
 import FilterDropDown from '../../../ui/FilterDropDown';
+import Filter from '../../../ui/Filter';
 import useCategories from '../../../hooks/useCategories';
 
+
+const sortOptions = [
+  {
+    label: 'مرتب سازی (جدیدترین)',
+    value: 'latest'
+  },
+  {
+    label: 'مرتب سازی (قدیمی ترین)',
+    value: 'earliest'
+  },
+];
+
+const statusOptions = [
+  {
+    label: 'همه',
+    value: 'ALL'
+  },
+  {
+    label: 'باز',
+    value: 'OPEN'
+  },
+  {
+    label: 'بسته',
+    value: 'CLOSE'
+  }
+];
 
 function ProjectsHeader() {
   const { transformedCategories } = useCategories()
   return(
     <div className='flex items-center justify-between text-secondary-700 mb-8'>
       <h2 className='text-lg font-bold'>لیست پروژه ها</h2>
-      <div>
+      <div className='flex gap-x-8 items-center'>
+        <Filter filterField='status' options={statusOptions} />
+        <FilterDropDown filterField='sort' options={sortOptions} />
         <FilterDropDown 
           filterField='category' 
           options={[
