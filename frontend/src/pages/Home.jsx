c15685b3c339bab99552f022c5d6bbcfefcb24ec
Azim Hatami <azimhatami4@gmail.com>
2025-01-15 21:43:56 +0330

    Display all projects on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router"
+
 import useUser from '../features/authentication/useUser';
 import useProjects from '../hooks/useProjects';
 import Loading from '../ui/Loading';
@@ -17,18 +19,24 @@ const projectStatus = {
 function Home() {
   const { isLoding: userLoading, user } = useUser();
   const { isLoading: projectsLoading, projects } = useProjects();
-  console.log(projects)
 
   return(
-    <div className='h-screen bg-secondary-0'>
-      <div className='container pt-8 xl:max-w-screen-xl flex items-center justify-between'>
-        <h2 className='text-lg font-bold text-secondary-500'>خانه</h2>
+    <div className='container h-screen bg-secondary-0 m-auto'>
+      <div className='pt-8 xl:max-w-screen-xl flex items-center justify-between'>
+        <div className='flex items-center gap-x-8'>
+          <NavLink to='/'>
+            <h2 className='text-lg font-bold text-secondary-500'>خانه</h2>
+          </NavLink>
+          <NavLink to='/admin/dashboard'>
+            <p className='text-lg font-bold text-secondary-500'>داشبورد</p>
+          </NavLink>
+        </div>
         { userLoading ? <Loading /> : (
           <p className='text-lg forn-bold text-secondary-600'>{user?.name} به وبسایت FreelancerHub خوش امدید</p>
         )}
       </div>
-      <div>
-        <div className='container mt-16 flex items-center justify-between flex-wrap gap-y-5'>
+      <div className=''>
+        <div className='container mt-16 flex items-center justify-normal flex-wrap gap-y-5 gap-x-6'>
           {projectsLoading ? <Loading /> : (
             projects.map((project) => {
               return(
@@ -38,7 +46,7 @@ function Home() {
                   <span className={`badge ${projectStatus[project.status].className}`}>{projectStatus[project.status].label}</span>
                   </div>
                   <p className='text-secondary-500 text-sm'>{project.description}</p>
-                  <div className='mt-6 flex items-center justify-stretch gap-x-2'>{project.tags.map((t) => <span className='badge badge-secondary'>{t}</span>)}</div>
+                  <div className='mt-6 flex items-center justify-stretch gap-x-2'>{project.tags.map((t) => <span key={t} className='badge badge-secondary'>{t}</span>)}</div>
                 </div>
               );
             })
