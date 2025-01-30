716f9e018a01b3276f320a8b57c099754e9320ee
Azim Hatami <azimhatami.dev@gmail.com>
2025-01-30 13:17:32 +0330

    Fix: incorrect toast message on user login

--- backend/app/http/controllers/userAuth.controller.js
+++ backend/app/http/controllers/userAuth.controller.js
@@ -62,7 +62,7 @@ class userAuthController extends Controller {
     // await setAuthCookie(res, user); // set httpOnly cookie
     await setAccessToken(res, user);
     await setRefreshToken(res, user);
-    let WELLCOME_MESSAGE = `کد تایید شد، به فرانت هوکس خوش آمدید`;
+    let WELLCOME_MESSAGE = `کد تایید شد، به فریلنسر هاب خوش آمدید`;
     if (!user.isActive)
       WELLCOME_MESSAGE = `کد تایید شد، لطفا اطلاعات خود را تکمیل کنید`;
 
